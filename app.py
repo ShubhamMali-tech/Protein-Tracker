46782464401ea356f77e6b5f57eba7e0562b33d8
SHUBHAM MALI <shubhammali11060@gmail.com>
2026-09-03 22:59:01 +0530

    setup page route

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/setup")
+def setup():
+    return render_template("setup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
